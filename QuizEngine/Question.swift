96ec42a5b5bbdc04c48bef5f2b095bbe807d2d69
brettawaytoday <brettawaytoday@gmail.com>
2021-03-11 10:39:16 +1300

    Removed Question Equatable custom implementation.

--- QuizEngine/Question.swift
+++ QuizEngine/Question.swift
@@ -22,15 +22,4 @@ public enum Question<T: Hashable> : Hashable {
             return value.hashValue
         }
     }
-    
-    public static func == (lhs: Question<T>, rhs: Question) -> Bool {
-        switch (lhs, rhs) {
-        case (.singleAnswer(let a), .singleAnswer(let b)):
-            return a == b
-        case (.multipleAnswer(let a), .multipleAnswer(let b)):
-            return a == b
-        default:
-            return false
-        }
-    }
 }
