be33acba0ac5dc6d733d2ed7610981398c0272c0
brettawaytoday <brettawaytoday@gmail.com>
2021-03-11 10:36:08 +1300

    Changed the Result extension in the tests to have a static method to make a result, to remove the public initialiser from the Result struct.

--- QuizEngine/Result.swift
+++ QuizEngine/Result.swift
@@ -10,14 +10,4 @@ import Foundation
 public struct Result<Question: Hashable, Answer>{
     public let answers: [Question: Answer]
     public let score: Int
-    
-    
-    /// Public initialiser primarily to help with building robust tests. This can be removed at a later stage if a better implementation is found.
-    /// - Parameters:
-    ///   - answers: Answers in the form of a Question with it's corresponding answers.
-    ///   - score: Score holder to keep track of the score.
-    public init(answers: [Question: Answer], score: Int) {
-        self.answers = answers
-        self.score = score
-    }
 }
